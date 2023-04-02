3877e1b84a48804317533f7a8505086aee5b7f96
PScoriae <pierreccesario@gmail.com>
2023-04-02 22:33:27 +0800

    feat: add user grpc server

--- cmd/user/main.go
+++ cmd/user/main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"jikkaem/internal/services/user/server"
+	pb "jikkaem/internal/shared/proto/user"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 6000))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterUserServer(s, &server.UserServer{})
+
+	log.Printf("User server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to server: %v", err)
+	}
+}
---
+++ internal/services/user/server/server.go
@@ -0,0 +1,44 @@
+package server
+
+import (
+	"context"
+
+	svc "jikkaem/internal/services/user/service"
+	"jikkaem/internal/shared/models"
+	pb "jikkaem/internal/shared/proto/user"
+)
+
+type UserServer struct {
+	pb.UnimplementedUserServer
+}
+
+func (s *UserServer) GetUserById(ctx context.Context, id *pb.ID) (*pb.UserObjectWithID, error) {
+	result, err := svc.GetUserById(id.GetId())
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.UserObjectWithID{
+		Id:    result.ID.String(),
+		Name:  result.User.Name,
+		Email: result.User.Email,
+	}, nil
+}
+
+func (s *UserServer) CreateUser(ctx context.Context, user *pb.UserObject) (*pb.UserObjectWithID, error) {
+	mappedUser := &models.User{
+		Name:  user.Name,
+		Email: user.Email,
+	}
+
+	result, err := svc.CreateUser(mappedUser)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.UserObjectWithID{
+		Id:    result.ID.String(),
+		Name:  result.User.Name,
+		Email: result.User.Email,
+	}, nil
+}
