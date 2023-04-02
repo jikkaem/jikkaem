04ae1a7395429f4670b6abebebd5ad72b944c6bb
PScoriae <pierreccesario@gmail.com>
2023-04-02 21:20:25 +0800

    feat: init user service

--- internal/services/user/service.go
+++ internal/services/user/service.go
@@ -1,11 +1,45 @@
 package user_service
 
-import "jikkaem/internal/shared/mongodb"
+import (
+	"context"
+	"fmt"
+	"jikkaem/internal/shared/models"
+	"jikkaem/internal/shared/mongodb"
 
-func GetUser(id string) {
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func GetUserById(id string) (*models.UserWithID, error) {
 	userColl, err := mongodb.GetColl("users")
+	if err != nil {
+		return nil, err
+	}
+	var result models.UserWithID
+	filter := bson.D{{Key: "_id", Value: id}}
+	if err = userColl.FindOne(context.TODO(), filter).Decode(&result); err != nil {
+		if err == mongo.ErrNoDocuments {
+			fmt.Println("No document found.")
+		}
+		panic(err)
+	}
+	return &result, nil
+}
+
+func CreateUser(user *models.User) (*models.UserWithID, error) {
+	userColl, err := mongodb.GetColl("users")
+	if err != nil {
+		return nil, err
+	}
+
+	userWithID := &models.UserWithID{
+		ID:   primitive.NewObjectID(),
+		User: user,
+	}
+	_, err = userColl.InsertOne(context.TODO(), userWithID)
 	if err != nil {
 		panic(err)
 	}
-	_ = userColl
+	return userWithID, nil
 }
---
+++ internal/shared/models/models.go
@@ -0,0 +1,48 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	Name  string `bson:"name"`
+	Email string `bson:"email"`
+}
+
+type UserWithID struct {
+	ID   primitive.ObjectID `bson:"_id"`
+	User *User
+}
+
+type Artist struct {
+	StageName       string    `bson:"stageName"`
+	FullName        string    `bson:"fullName"`
+	KoreanName      string    `bson:"koreanName"`
+	KoreanStageName string    `bson:"koreanStageName"`
+	DOB             time.Time `bson:"dob,omitempty"`
+	Group           string    `bson:"group,omitempty"`
+	Country         string    `bson:"country"`
+	Height          int8      `bson:"height,omitempty"`
+	Weight          int8      `bson:"weight,omitempty"`
+	Birthplace      string    `bson:"birthplace"`
+	Gender          string    `bson:"gender"`
+	Instagram       string    `bson:"instagram,omitempty"`
+}
+
+type ArtistWithID struct {
+	ID primitive.ObjectID `bson:"_id"`
+	*Artist
+}
+
+type Fancam struct {
+	Title   string   `bson:"title"`
+	YtLink  string   `bson:"ytLink"`
+	Artists []Artist `bson:"artists"`
+}
+
+type FancamWithID struct {
+	ID primitive.ObjectID `bson:"_id"`
+	*Fancam
+}
