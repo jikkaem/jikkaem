8a7adc55c5ae6a91e2fe4526e701b1797fe0c381
PScoriae <pierreccesario@gmail.com>
2023-04-11 13:16:52 +0800

    feat: add working py script

--- internal/services/scraper/main.py
+++ internal/services/scraper/main.py
@@ -1,17 +1,26 @@
 import os
+import grpc
+import fancam_pb2
+import fancam_pb2_grpc
+from google.protobuf.timestamp_pb2 import Timestamp
 from dotenv import load_dotenv
-
-load_dotenv()
-from pymongo import MongoClient
 from datetime import datetime
-
 from googleapiclient.discovery import build
 
-scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
+load_dotenv()
 
 
-# Filter out data that we don't need
+def tstamp_from_dt(dt):
+    """Takes in a datetime variable dt and returns a protobuf-compatible timestamp variable"""
+    ts = Timestamp()
+    ts.FromDatetime(dt)
+    return ts
+
+
 def massageData(data):
+    """Takes in a JSON object from the list "items" returned by
+    the search endpoint of YouTube Date API v3
+    """
     # Set shortcut
     s = data["snippet"]
 
@@ -32,35 +41,22 @@ def massageData(data):
         "channelId": s["channelId"],
         "channelTitle": s["channelTitle"],
         "rootThumbnail": rootThumbnail,
-        "recordDate": None,
-        "artists": [],
+        "recordDate": dtobj,
         "suggestedTags": [],
     }
 
 
-client = MongoClient("mongodb://localhost:10001/")
-
-
-def insertIntoDb(data):
-    db = client["jikkaem"]
-    coll = db["fancams"]
-
-    coll.insert_many(data)
-
-
-def main():
+def scrapeTitles(artist):
     api_service_name = "youtube"
     api_version = "v3"
     developerKey = os.environ["YT_API_KEY"]
 
     youtube = build(api_service_name, api_version, developerKey=developerKey)
 
-    focus = "Tzuyu"
-
     request = youtube.search().list(
         part="snippet",
-        maxResults=50,
-        q="tzuyu fancam",
+        maxResults=2,
+        q=f"{artist} fancam",
         type="video",
         videoEmbeddable="true",
     )
@@ -70,9 +66,55 @@ def main():
 
     massagedData = list(map(massageData, items))
 
-    insertIntoDb(massagedData)
+    return massagedData
 
-    print("done")
+
+def insertFancams(fancamList: dict, suggested_tags) -> None:
+    print(fancamList)
+    # Connects to Fancam microservice and creates fancams
+    with grpc.insecure_channel("localhost:6001") as channel:
+        stub = fancam_pb2_grpc.FancamStub(channel)
+        fancams = []
+        for document in fancamList:
+            suggestedTags = fancam_pb2.SuggestedTags(
+                en_artist=["set"],
+                en_group=["sldf"],
+                en_song=[";alksdf"],
+                kr_artist=["l;askdjf"],
+                kr_group=["laksdf"],
+                kr_song=["sldfh"],
+            )
+
+            fancam = fancam_pb2.FancamObject(
+                id=document["videoId"],
+                title=document["title"],
+                description=document["description"],
+                publishedAt=tstamp_from_dt(document["publishedAt"]),
+                channelId=document["channelId"],
+                channelTitle=document["channelTitle"],
+                rootThumbnail=document["rootThumbnail"],
+                record_date=tstamp_from_dt(document["recordDate"]),
+                suggested_tags=suggestedTags,
+            )
+            fancams.append(fancam)
+
+        print(fancams)
+        print("Attempting to insert fancams...")
+        res = stub.CreateFancams(fancam_pb2.FancamList(fancams=fancams))
+        print("Successfully inserted fancams")
+
+
+def main():
+    # Read file with all artists and groups
+    with open("groupsAndArtists.txt") as f:
+        line = f.readline()
+
+        artists = line.split(",")
+
+        for artist in artists:
+            titles = scrapeTitles(artist)
+
+            insertFancams(titles, "hi")
 
 
 if __name__ == "__main__":
